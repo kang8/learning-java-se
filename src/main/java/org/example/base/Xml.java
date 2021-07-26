83de552f163a559ab5e7ddc61db78be574b51b2f
kang <1115610574@qq.com>
2021-07-26 21:26:44 +0800

    use xpath get "/project/version"

--- src/main/java/org/example/base/Xml.java
+++ src/main/java/org/example/base/Xml.java
@@ -1,10 +1,15 @@
 package org.example.base;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.net.URL;
 
@@ -15,6 +20,15 @@ public class Xml {
         Xml xml = new Xml();
         try {
             Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xml.resource.getFile());
+
+            XPath xPath = XPathFactory.newInstance().newXPath();
+            try {
+                Node versionNode = (Node) xPath.compile("/project/version").evaluate(doc, XPathConstants.NODE);
+                String version = versionNode.getTextContent();
+                System.out.println(version);
+            } catch (XPathExpressionException e) {
+                e.printStackTrace();
+            }
         } catch (SAXException e) {
             e.printStackTrace();
         } catch (IOException e) {
